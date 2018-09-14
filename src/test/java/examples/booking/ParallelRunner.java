f5ccd569036e8b0c308bd9b7ea6ae604554390d3
cuhavp <dtha@fossil.com>
2018-09-14 17:13:58 +0700

    add cucumber html report

--- src/test/java/examples/booking/ParallelRunner.java
+++ src/test/java/examples/booking/ParallelRunner.java
@@ -1,4 +1,36 @@
 package examples.booking;
 
+import com.intuit.karate.cucumber.CucumberRunner;
+import com.intuit.karate.cucumber.KarateStats;
+import cucumber.api.CucumberOptions;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+//@CucumberOptions(tags = {"~@ignore"})
 public class ParallelRunner {
+
+    @Test
+    public void testParallel() {
+        KarateStats stats = CucumberRunner.parallel(getClass(), 5);
+        generateReport(stats.getReportDir());
+        assertTrue("there are scenario failures", stats.getFailCount() == 0);
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "Booking Rest API");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 }
